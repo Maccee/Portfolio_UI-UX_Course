284f3f555ca8cc1f44865b2fbb87024e01a1927c
Macce <makkeeh@gmail.com>
2023-10-03 18:14:22 +0300

    build7

--- src/App.jsx
+++ src/App.jsx
@@ -31,9 +31,8 @@ function App() {
 
   useEffect(() => {
     const navbar = document.getElementById("navbar");
-    const navmain = document.getElementById("navmain");
-    const main = document.getElementById("home-section");
-    const firstP = document.querySelector("#home-section p");
+    
+    
 
     let sticky = false; // to track the state
 
@@ -50,23 +49,14 @@ function App() {
         // Change navbar styles
         navbar.style.flexDirection = "row";
         navbar.style.backgroundColor = "rgba(0,0,0,0.8)";
-        navbar.style.height = "50px";
-        main.style.textAlign = "center";
-        main.style.width = "100%";
-        firstP.style.textAlign = "center";
-        // Remove flex from #navmain
-        navmain.style.display = "block";
+        
+        
+        
         sticky = true;
       } else if (window.scrollY < navbar.offsetTop && sticky) {
-        // Revert navbar styles
-        navbar.style.flexDirection = "column";
-        navbar.style.height = "auto";
+       
         navbar.style.backgroundColor = "transparent";
-        main.style.textAlign = "left";
-        main.style.width = "100%";
-        firstP.style.textAlign = "left";
-        // Add flex back to #navmain
-        navmain.style.display = "flex";
+       
         sticky = false;
       }
     };
@@ -86,6 +76,7 @@ function App() {
 
   function scrollToSection(id) {
     document.getElementById(id).scrollIntoView({
+      
       behavior: "smooth",
     });
   }
@@ -103,63 +94,34 @@ function App() {
           <img src={logo} alt="logo" />
         </header>
 
-        <div id="navmain">
-          <nav className="navbar" id="navbar">
-            <img src={home} alt="Home Icon" onClick={scrollToTop} />
-            <img
-              src={portfolioIcon}
-              alt="Portfolio Icon"
-              onClick={() => scrollToSection("projects-section")}
-            />
-            <img
-              src={skill}
-              alt="Skill Icon"
-              onClick={() => scrollToSection("skills-section")}
-            />
-            <img src={user} alt="User Icon" onClick={() => scrollToFooter()} />
-          </nav>
-          <main id="home-section">
-            <h1>Welcome to my Portfolio</h1>
-            <h2>&lt;!-- Engineering Seamless Digital Solutions. --&gt;</h2>
+        <nav className="navbar" id="navbar">
+          <img src={home} alt="Home Icon" onClick={scrollToTop} />
+          <img
+            src={portfolioIcon}
+            alt="Portfolio Icon"
+            onClick={() => scrollToSection("projects-section")}
+          />
+          <img
+            src={skill}
+            alt="Skill Icon"
+            onClick={() => scrollToSection("skills-section")}
+          />
+          <img src={user} alt="User Icon" onClick={() => scrollToFooter()} />
+        </nav>
+        <main id="home-section">
+          <h1>Welcome to my Portfolio</h1>
+          <h2>&lt;!-- Engineering Seamless Digital Solutions. --&gt;</h2>
 
-            <p>
-              I'm Marcus, an enthusiastic digital engineer driven by innovation
-              and creativity. Despite being at the beginning of my professional
-              journey, I possess a solid foundation in digital technologies and
-              a fervent desire to revolutionize how businesses operate in the
-              digital realm. My portfolio reflects my passion and dedication,
-              showcasing projects that highlight my ability to create
-              transformative digital solutions.
-            </p>
-            <p>
-              I'm Marcus, an enthusiastic digital engineer driven by innovation
-              and creativity. Despite being at the beginning of my professional
-              journey, I possess a solid foundation in digital technologies and
-              a fervent desire to revolutionize how businesses operate in the
-              digital realm. My portfolio reflects my passion and dedication,
-              showcasing projects that highlight my ability to create
-              transformative digital solutions.
-            </p>
-            <p>
-              I'm Marcus, an enthusiastic digital engineer driven by innovation
-              and creativity. Despite being at the beginning of my professional
-              journey, I possess a solid foundation in digital technologies and
-              a fervent desire to revolutionize how businesses operate in the
-              digital realm. My portfolio reflects my passion and dedication,
-              showcasing projects that highlight my ability to create
-              transformative digital solutions.
-            </p>
-            <p>
-              I'm Marcus, an enthusiastic digital engineer driven by innovation
-              and creativity. Despite being at the beginning of my professional
-              journey, I possess a solid foundation in digital technologies and
-              a fervent desire to revolutionize how businesses operate in the
-              digital realm. My portfolio reflects my passion and dedication,
-              showcasing projects that highlight my ability to create
-              transformative digital solutions.
-            </p>
-          </main>
-        </div>
+          <p>
+            I'm Marcus, an enthusiastic digital engineer driven by innovation
+            and creativity. Despite being at the beginning of my professional
+            journey, I possess a solid foundation in digital technologies and a
+            fervent desire to revolutionize how businesses operate in the
+            digital realm. My portfolio reflects my passion and dedication,
+            showcasing projects that highlight my ability to create
+            transformative digital solutions.
+          </p>
+        </main>
 
         <section id="projects-section">
           <h3>Featured Projects</h3>
